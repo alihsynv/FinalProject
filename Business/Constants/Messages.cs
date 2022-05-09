33de4fffa7b3295b48d3fe3baee4288a11261d80
alihsynv <alihsynov@gmail.com>
2022-05-09 17:38:21 +0300

    09.05.2022 Lesson 10

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product added";
+        public static string ProductNameInvalid = "The product name is invalid.";
+        public static string MaintenanceTime = "Maintenance time";
+        public static string ProductListed = "Product listed";
+    }
+}
